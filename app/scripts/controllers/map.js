ffd43eb63b170adc8f5fa673be0227c52523c11c
Brandon Miller <brandonevanmiller@gmail.com>
2015-04-12 15:41:57 -0400

    Added some points and updated map

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -4,7 +4,7 @@
  * @ngdoc function
  * @name angularMaterialPracticeApp.controller:MapCtrl
  * @description
- * # AboutCtrl
+ * # MapCtrl
  * Controller of the angularMaterialPracticeApp
  */
 angular.module('angularMaterialPracticeApp')
@@ -18,5 +18,27 @@ angular.module('angularMaterialPracticeApp')
     var mapVM = this;
 
     console.log("Maping")
-    
+
+    var map = L.map('map').setView([35.128298, -82.380166], 15)
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var scoutCraft = new L.marker([35.129420, -82.380560]);
+    var firstAid = new L.marker([35.130016, -82.380427]);
+    var firstYear = new L.marker([35.130135, -82.380753]);
+    var civDev = new L.marker([35.130268, -82.380157]);
+    var range = new L.marker([35.131160, -82.381065]);
+    var flagPole = new L.marker([35.129494, -82.379192]);
+    var diningHall = new L.marker([35.129690, -82.378761]);
+
+    scoutCraft.addTo(map);
+    firstAid.addTo(map);
+    firstYear.addTo(map);
+    civDev.addTo(map);
+    range.addTo(map);
+    flagPole.addTo(map);
+    diningHall.addTo(map);
+
   });
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -26,6 +26,8 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-aria/angular-aria.js',
+      'bower_components/angular-material/angular-material.js',
       'bower_components/leaflet/dist/leaflet.js',
       'bower_components/leaflet/dist/leaflet-src.js',
       'bower_components/angular-mocks/angular-mocks.js',
